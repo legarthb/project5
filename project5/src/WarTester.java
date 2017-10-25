f77abcb359d2a53f46d1a98c55edf7b3836351e6
legarthb <legarthb20@mail.wlu.edu>
2017-10-24 21:58:19 -0400

    Update WarTester.java

--- project5/src/WarTester.java
+++ project5/src/WarTester.java
@@ -1,7 +1,7 @@
 
 public class WarTester{
     public static void main(String[] args) {
-     	  WarGame game = new WarGame();
+     	WarGame game = new WarGame();
         game.deal();
         while (! game.winner()) {
             game.Step();
