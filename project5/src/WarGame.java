2b5ea85b27e44936cfd8e8d3784820c3b2c95eac
legarthb <legarthb20@mail.wlu.edu>
2017-10-24 21:19:28 -0400

    WarGame edited, needs documentation

--- project5/src/WarGame.java
+++ project5/src/WarGame.java
@@ -1,4 +1,59 @@
+import java.util.*;
 
-public class WarGame {
-
+public class WarGame{
+    
+    //Creating the players
+    private Player playerOne;
+    private Player playerTwo;
+    private List<Card> warPile = new ArrayList<Card>();
+    private String gameState = "";
+    private Deck deck;
+    public WarGame() {
+    	    deck.shuffle();
+    }
+    public String ToString() {
+        return gameState;
+    }
+    public void deal() {
+        while (! this.deck.isEmpty()){
+            this.playerOne.addToUnplayedPile(this.deck.deal());
+            this.playerTwo.addToUnplayedPile(this.deck.deal());
+        }
+    }
+    public void Step() {
+        Card cardOne = this.playerOne.getCard();
+        Card cardTwo = this.playerTwo.getCard();
+        this.warPile.add(cardOne);
+        this.warPile.add(cardTwo);
+        this.gameState = "Player 1: " + cardOne.toString() + "\n" + "Player 2: " + cardTwo.toString();
+        if (cardOne.getRank() == cardTwo.getRank())
+            this.gameState += "\nCards added to War pile\n";
+        else if (cardOne.getRank() > cardTwo.getRank()) {
+            this.transferCards(playerOne);
+            this.gameState = "\nCards go to Player 1\n";
+        }
+        else {
+            this.transferCards(playerTwo);
+            this.gameState += "\nCards go to Player 2";
+        }
+    }
+    public void transferCards(Player player) {
+    	    player.addToWinningsPile(this.warPile.remove(0));
+    }
+    public void winner() {
+        if (this.playerOne.isDone() || this.playerTwo.isDone()) {
+            int countOne = this.playerOne.winningsCount();
+            int countTwo = this.playerTwo.winningsCount();
+            if (countOne > countTwo) {
+                System.out.println("Player 1 wins, " + countOne + " to " + countTwo + "!");
+            }
+            else if (countTwo > countOne) {
+                System.out.println("Player 2 wins, " + countTwo + " to " + countOne + "!");
+            }
+            else {
+                System.out.println("The game ends in a tie!");
+            }
+        }
+    }
 }
+
