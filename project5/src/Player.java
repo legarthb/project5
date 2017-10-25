1d081c631d76b38c14b0d615228dc99f62fea321
legarthb <legarthb20@mail.wlu.edu>
2017-10-24 21:07:50 -0400

    finished player class needs documentation

--- project5/src/Player.java
+++ project5/src/Player.java
@@ -1,4 +1,26 @@
+import java.util.*;
 
-public class Player {
-
+public class Player extends Deck{
+	
+	private ArrayList<Card> unplayedPile = new ArrayList<Card>();
+	private ArrayList<Card> winningPile = new ArrayList<Card>();
+	
+	public Player() {
+		super();
+	}
+	public void addToUnplayedPile(Card c) {
+		this.unplayedPile.add(0, c);
+	}
+	public void addToWinningsPile(Card c) {
+		this.winningPile.add(0, c);
+	}
+	public Card getCard() {
+		return unplayedPile.remove(0);
+	}
+	public boolean isDone() {
+		return unplayedPile.size() == 0;
+	}
+	public int winningsCount() {
+		return winningPile.size();
+	}
 }
