120167af1b3ca8a2b9b11d8bfe1364824a02b517
Mark <Mark@Mark-Donohue-PC.ad.wlu.edu>
2017-10-24 20:54:02 -0400

    edited player class

--- project5/src/Player.java
+++ project5/src/Player.java
@@ -1,4 +1,14 @@
+import java.util.LinkedList;
 
-public class Player {
+publicLinkedList<E>yer {
+	private final LinkedList<Card> deck;
+	private string name;
 
+	public Player(String name, LinkedList<Card> deck) {
+		this.name = name;
+		this.deck = deck;
+	}
+	
+	public boolean hasCard() {
+		return !deck.isEmpty();
 }
