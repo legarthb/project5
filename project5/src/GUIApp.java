2b1444ad3bc6955a231acf942d4853a11054b064
Tommy <thetfordt18@mail.wlu.edu>
2017-10-26 11:14:46 -0400

    Added author names, started documenting MainView.java, still needs more documentation in the constructor.

--- project5/src/GUIApp.java
+++ project5/src/GUIApp.java
@@ -4,15 +4,20 @@ import javax.swing.JFrame;
  * Generic main method template for any GUI-based application.
  * Instantiates a model and passes it to a new view.
  * @author lambertk
- *
+ * @author Brian Legarth
+ * @author Tommy Thetford
+ * @author Chris Randall
+ * @author Mark Donohue
  */
+
+
 public class GUIApp{
 
     public static void main(String[] args){
         final Deck deck = new Deck();
         final JFrame view = new MainView(deck);
         view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        view.setTitle("tHe GamE oF WaR");
+        view.setTitle("The Game of War");
         view.setSize(600, 350);
         view.setResizable(false);
         view.setVisible(true);
